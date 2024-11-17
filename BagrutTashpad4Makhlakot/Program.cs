e776f6d39a5791c26f3ee565933ecac3c5e6c853
helarye <helarye@gmail.com>
2024-11-17 09:15:15 +0200

    Add project files.

---
+++ BagrutTashpad4Makhlakot/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BagrutTashpad4Makhlakot;
+
+Console.WriteLine("Hello, World!");
+TourPackage myPac = new TourPackage(0,100,50,10);
+Console.WriteLine(myPac);
+
+//(1)תשפד שאלה 4 סעיף ב
+static int Calculate(TourPackage[] packages)
+
+//הפעולה מקבלת מערך חבילות תקשורת ומחזירה את כמות החריגות שהייתה בהם חריגה
+{
+    int kamutKhorgim = 0;
+    for (int i = 0; i < packages.Length; i++)
+    {
+        if (packages[i].GetExtra() > 0) { kamutKhorgim++; }
+    }
+    return kamutKhorgim;
+}
+//(2)תשפד שאלה 4 סעיף ב
+static int[] Customers(TourPackage[] packages)
+{
+    int kamutHorgim = Calculate(packages);
+    int[] khorgimCust= new int[kamutHorgim];
+    return khorgimCust;
+}
+
+
---
+++ BagrutTashpad4Makhlakot/TourPackage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BagrutTashpad4Makhlakot
+{
+    public class TourPackage
+    {
+        private int id;
+        private int price;
+        private int maxTime;
+        private int maxData;
+        private int extra;
+
+        public TourPackage(int id, int price, int maxTime, int maxData)
+        {
+            this.id = id;
+            this.price = price;
+            this.maxTime = maxTime;
+            this.maxData = maxData;
+            this.extra = 0;
+        }
+        public void SetExtra(int minutes, int usage)
+        //הפונקציה מקבלת את נפח השימוש בדטה ואת דקות השיחה ומחשבת את עלות החריגה
+        {
+            this.extra= minutes + usage*2;
+        }
+        public override string ToString()
+        {
+            string result="Package ditails for id:";
+            result+=id.ToString();
+            result+=" Price: "+ price+" Minutes:"+maxTime+" Data: "+maxData;
+            result += " Extra Pay: " + this.extra;
+            return result;
+        }
+        public int GetExtra() { return this.extra; }
+
+    }
+}
